93d7bf106580d7d6d1cb6cd6e756d11f0ee4545a
Tyler Quast <TQuast@fanatics.com>
2022-06-07 07:14:17 -0700

    submitting

--- concurrency/concur/advanced_pool.go
+++ concurrency/concur/advanced_pool.go
@@ -1,4 +1,4 @@
-package concurrency
+package con
 
 import (
 	"context"
--- concurrency/concur/simple_pool.go
+++ concurrency/concur/simple_pool.go
@@ -1,6 +1,8 @@
 // Package concurrency implements worker pool interfaces, one simple and one a
 // bit more complex.
-package concurrency
+package con
+
+import "fmt"
 
 // SimplePool is a simple worker pool that does not support cancellation or
 // closing. All functions are safe to call from multiple goroutines.
@@ -11,9 +13,31 @@ type SimplePool interface {
 	Submit(func())
 }
 
+type worker struct {
+	id int
+	jobs chan func()
+}
+
 // NewSimplePool creates a new SimplePool that only allows the given maximum
 // concurrent tasks to run at any one time. maxConcurrent must be greater than
 // zero.
 func NewSimplePool(maxConcurrent int) SimplePool {
-	panic("TODO")
+	jobs := make(chan func(), maxConcurrent)
+
+	for w := 1; w <= maxConcurrent; w++ {
+		go doWork(w, jobs)
+	}
+	return worker{jobs: jobs}
+}
+
+
+func (w worker) Submit(f func()) {
+	w.jobs <- f
+}
+
+func doWork(id int, jobs <-chan func()) {
+	for f := range jobs {
+		fmt.Printf("Worker %d", id)
+		f()
+	}
 }
